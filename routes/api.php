2686e03bb0b9b47c5dc0cb84b15e94da4305ef58
mykumykuni <itsmyku@gmail.com>
2026-01-22 20:36:15 +0800

    Added API test routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return ['status' => 'Success', 'message' => 'Your API is working!'];
+});
